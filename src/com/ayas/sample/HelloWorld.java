718297d8bb0071436eccb633994e2875ae3ba3b7
Ayaskant Swain <ayaskant.swain@gmail.com>
2015-04-27 14:53:04 +0530

    Commiting My First Java Code
    Committing My First Java Code Hello World

---
+++ src/com/ayas/sample/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.ayas.sample;
+
+/**
+ * @author Ayaskant Swain
+ * @since 27th-April-2015
+ * <br><br>
+ * My first GitHub java code
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!. This is my first code in GitHub repo.");
+		if(args.length > 0) {
+			System.out.println("Printing program arguments");
+			for(String str : args)
+				System.out.println(str);
+		}
+
+	}
+}
---
+++ src/com/ayas/sample/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author Ayaskant Swain
+ * @since 27th-April-2015
+ *
+ */
+package com.ayas.sample;
