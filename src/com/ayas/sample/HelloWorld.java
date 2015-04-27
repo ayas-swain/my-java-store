5675c23ca237ff2093c563fd20e90a70eebae654
Ayaskant Swain <ayaskant.swain@gmail.com>
2015-04-27 15:58:19 +0530

    My 2nd commit on HelloWorld program

--- src/com/ayas/sample/HelloWorld.java
+++ src/com/ayas/sample/HelloWorld.java
@@ -11,10 +11,13 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World !!!. This is my first code in GitHub repo.");
+		
 		if(args.length > 0) {
 			System.out.println("Printing program arguments");
 			for(String str : args)
 				System.out.println(str);
+		
+		System.out.println("Bye see you soon.");	
 		}
 
 	}
